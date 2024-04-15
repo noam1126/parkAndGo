ee7940479bff53d6f0205124c8a2bc45555c6d19
omerpeled6 <opeled6@gmail.com>
2024-04-15 14:53:39 +0300

    o

--- TempSign/app/src/main/java/com/example/tempsign/activitys/MainActivity.java
+++ TempSign/app/src/main/java/com/example/tempsign/activitys/MainActivity.java
@@ -35,17 +35,6 @@ public class MainActivity extends AppCompatActivity implements NavigationListene
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id=item.getItemId();
-        if(id==R.id.star){
-            Toast.makeText(this,"star",Toast.LENGTH_SHORT).show();
-        }
-        if(id==R.id.fav){
-            Toast.makeText(this,"heart",Toast.LENGTH_SHORT).show();
-        }
-        return true;
-    }
 
     @Override
     public void navigateToInfoParking(View v) {
